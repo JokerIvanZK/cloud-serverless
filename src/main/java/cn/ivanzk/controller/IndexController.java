b0b8bcc1f0072e49c3cee2343096dfd1dfb47460
zk <1184253234@qq.com>
2023-03-08 09:20:31 +0800

    添加MiraiBot状态查询

--- src/main/java/cn/ivanzk/controller/IndexController.java
+++ src/main/java/cn/ivanzk/controller/IndexController.java
@@ -1,8 +1,13 @@
 package cn.ivanzk.controller;
 
+import com.google.common.collect.Maps;
+import net.mamoe.mirai.Bot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @author zk
@@ -10,10 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 @RequestMapping("/")
 public class IndexController {
+    @Autowired
+    private Bot bot;
 
-    @RequestMapping(value = "/ping", method = {RequestMethod.GET, RequestMethod.POST})
+    @GetMapping(value = "/ping")
     public String verify() {
         return "pong";
     }
 
+    @GetMapping(value = "/state")
+    public Map<String, Object> state() {
+        Map<String, Object> map = Maps.newHashMap();
+        map.put("MiraiBot", bot.isOnline());
+        return map;
+    }
 }
