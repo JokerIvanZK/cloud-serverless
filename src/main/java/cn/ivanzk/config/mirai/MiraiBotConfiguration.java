11972b1ae1279d5f464f84cf1a5010179a6009e2
zk <1184253234@qq.com>
2023-03-19 08:19:13 +0800

    关闭无效httpclient

--- src/main/java/cn/ivanzk/config/mirai/MiraiBotConfiguration.java
+++ src/main/java/cn/ivanzk/config/mirai/MiraiBotConfiguration.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import xyz.cssxsh.mirai.tool.FixProtocolVersion;
-
-import java.util.Map;
 
 /**
  * QQ机器人配置
@@ -28,25 +25,15 @@ public class MiraiBotConfiguration {
     @Bean
     public Bot bot(MiraiBotProperties miraiBotProperties) {
         Bot bot = BotFactory.INSTANCE.newBot(miraiBotProperties.getQq(), miraiBotProperties.getPassword(), new BotConfiguration() {{
-            autoReconnectOnForceOffline();
             noNetworkLog();
             fileBasedDeviceInfo(miraiBotProperties.getDeviceInfoPath());
             setProtocol(miraiBotProperties.getProtocol());
+            autoReconnectOnForceOffline();
         }});
         bot.login();
         if (bot.isOnline()) {
             bot.getFriend(miraiBotProperties.getAdmin()).sendMessage(String.format("QQ:%s<%s>上线", bot.getNick(), bot.getId()));
         }
         return bot;
-    }
-
-    // 升级协议版本
-    public static void update() {
-        FixProtocolVersion.update();
-    }
-
-    // 获取协议版本信息 你可以用这个来检查update是否正常工作
-    public static Map<BotConfiguration.MiraiProtocol, String> info() {
-        return FixProtocolVersion.info();
     }
 }
