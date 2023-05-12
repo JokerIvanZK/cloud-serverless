470535e7020f7ac00e5611dc9060f3f6fdd01c65
zk <1184253234@qq.com>
2023-05-12 11:24:25 +0800

    修改定时任务内存溢出bug

--- src/main/java/cn/ivanzk/config/kook/KookClient.java
+++ src/main/java/cn/ivanzk/config/kook/KookClient.java
@@ -10,14 +10,20 @@ import com.java.comn.util.JacksonUtil;
 import com.java.comn.util.SmallTool;
 import com.net.comn.http.HttpClientProxy;
 import com.net.comn.server.ServerContext;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.util.EntityUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -75,20 +81,20 @@ public class KookClient {
             return;
         }
         channelCache.get(null).forEach(channel -> {
-            if (channel.getName().indexOf(channelName) >= 0 || channel.getName().indexOf(ZhTwConverterUtil.toTraditional(channelName)) >= 0) {
+            if (channel.getName().contains(channelName) || channel.getName().contains(ZhTwConverterUtil.toTraditional(channelName))) {
                 channelMessage(kookProperties, channel.getId(), formatMessage(message));
             }
         });
     }
 
-    public void channelImg(String channelName, String imgUrl) {
+    public void channelImg(String channelName, String... imgUrl) {
         KookProperties kookProperties = ServerContext.getBean(KookProperties.class);
         if (kookProperties == null) {
             return;
         }
         channelCache.get(null).forEach(channel -> {
-            if (channel.getName().indexOf(channelName) >= 0 || channel.getName().indexOf(ZhTwConverterUtil.toTraditional(channelName)) >= 0) {
-                channelMessage(kookProperties, channel.getId(), imgUrl);
+            if (channel.getName().contains(channelName) || channel.getName().contains(ZhTwConverterUtil.toTraditional(channelName))) {
+                channelCardImage(kookProperties, channel.getId(), channel.getName(), imgUrl);
             }
         });
     }
@@ -103,33 +109,56 @@ public class KookClient {
     }
 
     public static void channelMessage(KookProperties kookProperties, String channelId, String content) {
-        try {
-            String url = kookProperties.getBaseUrl() + kookProperties.getChannelMessage();
-            Map<String, Object> header = Maps.newHashMap();
-            header.put("Authorization", "Bot " + kookProperties.getToken());
-            MapWrap body = new MapWrap();
-            body.put("target_id", channelId);
-            body.put("content", content);
-            httpClientProxy.doPostJsonForResult(url, body, header);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String url = kookProperties.getBaseUrl() + kookProperties.getChannelMessage();
+        Map<String, Object> header = Maps.newHashMap();
+        header.put("Authorization", "Bot " + kookProperties.getToken());
+        MapWrap body = new MapWrap();
+        body.put("target_id", channelId);
+        body.put("content", content);
+        httpClientProxy.doPostJsonTryForResult(url, body, header);
         httpClientProxy.clearInvalidConnection();
     }
 
     public static void directMessage(KookProperties kookProperties, String userId, String content) {
+        String url = kookProperties.getBaseUrl() + kookProperties.getDirectMessage();
+        Map<String, Object> header = Maps.newHashMap();
+        header.put("Authorization", "Bot " + kookProperties.getToken());
+        MapWrap body = new MapWrap();
+        body.put("target_id", userId);
+        body.put("content", content);
+        httpClientProxy.doPostJsonTryForResult(url, body, header);
+        httpClientProxy.clearInvalidConnection();
+    }
+
+    public static void channelCardImage(KookProperties kookProperties, String channelId, String title, String... imgUrl) {
+        String url = kookProperties.getBaseUrl() + kookProperties.getChannelMessage();
+        Map<String, Object> header = Maps.newHashMap();
+        header.put("Authorization", "Bot " + kookProperties.getToken());
+        MapWrap body = new MapWrap();
+        body.put("type", 10);
+        body.put("target_id", channelId);
+        body.put("content", buildCardMessage(title, imgUrl));
+        httpClientProxy.doPostJsonTryForResult(url, body, header);
+        httpClientProxy.clearInvalidConnection();
+    }
+
+    public static String uploadFile(KookProperties kookProperties, String filePath) {
+        String url = kookProperties.getBaseUrl() + kookProperties.getUploadFile();
+        String jsonString = null;
         try {
-            String url = kookProperties.getBaseUrl() + kookProperties.getDirectMessage();
-            Map<String, Object> header = Maps.newHashMap();
-            header.put("Authorization", "Bot " + kookProperties.getToken());
-            MapWrap body = new MapWrap();
-            body.put("target_id", userId);
-            body.put("content", content);
-            httpClientProxy.doPostJsonForResult(url, body, header);
+            jsonString = httpClientProxy.doHttpPost(url, (httpClient, httpPost) -> {
+                httpPost.addHeader("Authorization", "Bot " + kookProperties.getToken());
+                httpPost.setEntity(MultipartEntityBuilder.create().addBinaryBody("file", new File(filePath)).build());
+                CloseableHttpResponse response = httpClient.execute(httpPost);
+                HttpEntity responseEntity = response.getEntity();
+                return EntityUtils.toString(responseEntity, "UTF-8");
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }
         httpClientProxy.clearInvalidConnection();
+        MapWrap result = JacksonUtil.fromJson(jsonString, MapWrap.class);
+        return result != null ? result.get("data.url", String.class) : null;
     }
 
     public static List<Guild> guildList() {
@@ -137,26 +166,21 @@ public class KookClient {
         if (kookProperties == null) {
             return Lists.newArrayList();
         }
-        try {
-            Map<String, Object> header = Maps.newHashMap();
-            header.put("Authorization", "Bot " + kookProperties.getToken());
-            String url = kookProperties.getBaseUrl() + kookProperties.getGuildList();
-            String jsonString = httpClientProxy.doGetForResult(url, header);
-            MapWrap result = JacksonUtil.fromJson(jsonString, MapWrap.class);
-            List<Map<String, Object>> list = result.get("data.items", GenericClazz.getArrayListClazz());
-            List<Guild> guilds = list.stream().map(s -> JacksonUtil.fromJson(JacksonUtil.toJson(s), Guild.class)).collect(Collectors.toList());
-            for (Guild guild : guilds) {
-                Guild guildView = guildView(guild.getId());
-                if (guildView != null) {
-                    guild.setChannels(guildView.getChannels());
-                }
+        Map<String, Object> header = Maps.newHashMap();
+        header.put("Authorization", "Bot " + kookProperties.getToken());
+        String url = kookProperties.getBaseUrl() + kookProperties.getGuildList();
+        String jsonString = httpClientProxy.doGetTryForResult(url, header);
+        MapWrap result = JacksonUtil.fromJson(jsonString, MapWrap.class);
+        List<Map<String, Object>> list = result.get("data.items", GenericClazz.getArrayListClazz());
+        List<Guild> guilds = list.stream().map(s -> JacksonUtil.fromJson(JacksonUtil.toJson(s), Guild.class)).collect(Collectors.toList());
+        for (Guild guild : guilds) {
+            Guild guildView = guildView(guild.getId());
+            if (guildView != null) {
+                guild.setChannels(guildView.getChannels());
             }
-            return guilds;
-        } catch (Exception e) {
-            e.printStackTrace();
         }
         httpClientProxy.clearInvalidConnection();
-        return Lists.newArrayList();
+        return guilds;
     }
 
     public static Guild guildView(String guildId) {
@@ -164,20 +188,15 @@ public class KookClient {
         if (kookProperties == null) {
             return null;
         }
-        try {
-            Map<String, Object> header = Maps.newHashMap();
-            header.put("Authorization", "Bot " + kookProperties.getToken());
-            String url = kookProperties.getBaseUrl() + kookProperties.getGuildView() + "?guild_id=" + guildId;
-            String jsonString = httpClientProxy.doGetForResult(url, header);
-            MapWrap result = JacksonUtil.fromJson(jsonString, MapWrap.class);
-            Object guildData = result.get("data");
-            Guild guild = JacksonUtil.fromJson(JacksonUtil.toJson(guildData), Guild.class);
-            return guild;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Map<String, Object> header = Maps.newHashMap();
+        header.put("Authorization", "Bot " + kookProperties.getToken());
+        String url = kookProperties.getBaseUrl() + kookProperties.getGuildView() + "?guild_id=" + guildId;
+        String jsonString = httpClientProxy.doGetTryForResult(url, header);
+        MapWrap result = JacksonUtil.fromJson(jsonString, MapWrap.class);
+        Object guildData = result.get("data");
+        Guild guild = JacksonUtil.fromJson(JacksonUtil.toJson(guildData), Guild.class);
         httpClientProxy.clearInvalidConnection();
-        return null;
+        return guild;
     }
 
     public static List<Channel> channelList(String guildId) {
@@ -185,23 +204,18 @@ public class KookClient {
         if (kookProperties == null) {
             return Lists.newArrayList();
         }
-        try {
-            String url = kookProperties.getBaseUrl() + kookProperties.getChannelList() + "?guild_id=" + guildId;
-            Map<String, Object> header = Maps.newHashMap();
-            header.put("Authorization", "Bot " + kookProperties.getToken());
-            String jsonString = httpClientProxy.doGetForResult(url, header);
-            MapWrap result = JacksonUtil.fromJson(jsonString, MapWrap.class);
-            List<Map<String, Object>> list = result.get("data.items", GenericClazz.getArrayListClazz());
-            List<Channel> channels = list.stream().map(s -> JacksonUtil.fromJson(JacksonUtil.toJson(s), Channel.class))
-                    .filter(s -> s != null)
-                    .filter(s -> s.getIs_category() == false)
-                    .collect(Collectors.toList());
-            return channels;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String url = kookProperties.getBaseUrl() + kookProperties.getChannelList() + "?guild_id=" + guildId;
+        Map<String, Object> header = Maps.newHashMap();
+        header.put("Authorization", "Bot " + kookProperties.getToken());
+        String jsonString = httpClientProxy.doGetTryForResult(url, header);
+        MapWrap result = JacksonUtil.fromJson(jsonString, MapWrap.class);
+        List<Map<String, Object>> list = result.get("data.items", GenericClazz.getArrayListClazz());
+        List<Channel> channels = list.stream().map(s -> JacksonUtil.fromJson(JacksonUtil.toJson(s), Channel.class))
+                .filter(Objects::nonNull)
+                .filter(s -> !s.getIs_category())
+                .collect(Collectors.toList());
         httpClientProxy.clearInvalidConnection();
-        return Lists.newArrayList();
+        return channels;
     }
 
     public static Channel channelView(String channelId) {
@@ -209,20 +223,41 @@ public class KookClient {
         if (kookProperties == null) {
             return null;
         }
-        try {
-            String url = kookProperties.getBaseUrl() + kookProperties.getChannelView() + "?target_id=" + channelId;
-            Map<String, Object> header = Maps.newHashMap();
-            header.put("Authorization", "Bot " + kookProperties.getToken());
-            String jsonString = httpClientProxy.doGetForResult(url, header);
-            MapWrap result = JacksonUtil.fromJson(jsonString, MapWrap.class);
-            Object guildData = result.get("data");
-            Channel channel = JacksonUtil.fromJson(JacksonUtil.toJson(guildData), Channel.class);
-            return channel;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String url = kookProperties.getBaseUrl() + kookProperties.getChannelView() + "?target_id=" + channelId;
+        Map<String, Object> header = Maps.newHashMap();
+        header.put("Authorization", "Bot " + kookProperties.getToken());
+        String jsonString = httpClientProxy.doGetTryForResult(url, header);
+        MapWrap result = JacksonUtil.fromJson(jsonString, MapWrap.class);
+        Object guildData = result.get("data");
+        Channel channel = JacksonUtil.fromJson(JacksonUtil.toJson(guildData), Channel.class);
         httpClientProxy.clearInvalidConnection();
-        return null;
+        return channel;
+    }
+
+    private static String buildCardMessage(String title, String... src) {
+        MapWrap cardImage = new MapWrap();
+        cardImage.put("type", "card");
+        cardImage.put("theme", "secondary");
+        cardImage.put("size", "lg");
+        List<MapWrap> modules = Lists.newArrayList();
+        MapWrap sectionModule = new MapWrap();
+        modules.add(sectionModule);
+        sectionModule.put("type", "section");
+        sectionModule.put("text.type", "plain-text");
+        sectionModule.put("text.content", title);
+        MapWrap imageModule = new MapWrap();
+        modules.add(imageModule);
+        cardImage.put("modules", modules);
+        List<MapWrap> elements = Lists.newArrayList();
+        imageModule.put("type", "container");
+        imageModule.put("elements", elements);
+        for (String s : src) {
+            MapWrap element = new MapWrap();
+            element.put("type", "image");
+            element.put("src", s);
+            elements.add(element);
+        }
+        return JacksonUtil.toFormatJson(Lists.newArrayList(cardImage));
     }
 
     private static String formatMessage(String message) {
--- src/main/java/cn/ivanzk/cron/KookCronJob.java
+++ src/main/java/cn/ivanzk/cron/KookCronJob.java
@@ -32,6 +32,7 @@ import javax.imageio.ImageReader;
 import javax.imageio.stream.ImageInputStream;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -74,92 +75,76 @@ public class KookCronJob {
     @Scheduled(cron = "0 0/5 * * * ?")
     public void updateMessage() throws InterruptedException {
         System.out.println("关闭无效连接:" + httpClientProxy.clearInvalidConnection());
-        UpdateInfo updateInfo = new UpdateInfo();
-        updateInfo.get();
+        KookClient kookClient = ServerContext.getBean(KookClient.class);
+        KookProperties kookProperties = ServerContext.getBean(KookProperties.class);
+        if (kookClient == null || kookProperties == null) {
+            return;
+        }
+
+        List<String> imgUrls = Lists.newArrayList();
+        HtmlParser htmlParser;
+        ImageRenderer imageRenderer;
+        for (String html : getHtmlString()) {
+            htmlParser = new HtmlParserImpl();
+            htmlParser.loadHtml(html);
+            imageRenderer = new ImageRendererImpl(htmlParser);
+            imageRenderer.saveImage("./1.png");
+
+            String imgUrl = KookClient.uploadFile(kookProperties, "./1.png");
+            if (SmallTool.notEmpty(imgUrl)) {
+                imgUrls.add(imgUrl);
+            }
+            Thread.sleep(5000);
+        }
+        kookClient.channelImg("更新", imgUrls.toArray(new String[0]));
         Thread.sleep(1000 * 60);
-        updateInfo = null;
         System.gc();
     }
 
-    private class UpdateInfo{
-        public void get() {
-            KookClient kookClient = ServerContext.getBean(KookClient.class);
-            KookProperties kookProperties = ServerContext.getBean(KookProperties.class);
-            if (kookClient == null || kookProperties == null) {
-                return;
-            }
-            try {
-                String url = "https://asia.archeage.com/news?lang=zh_TW";
-                String html = httpClientProxy.doGetForResult(url, Maps.newHashMap());
-                String pattern = "/news/{1}[\\d]+[?]{1}page={1}[\\d]+";
-                Matcher matcher = Pattern.compile(pattern).matcher(html);
+    private List<String> getHtmlString() {
+        List<String> list = Lists.newArrayList();
 
-                List<String> awaitPushUrl = Lists.newArrayList();
-                while (matcher.find()) {
-                    awaitPushUrl.add(0, matcher.group());
-                }
-                if (SmallTool.isEmpty(lastPushUrl)) {
-                    lastPushUrl = awaitPushUrl.get(awaitPushUrl.size() - 1);
-                }
-                int cutoff = 0;
-                for (int i = 0; i < awaitPushUrl.size(); i++) {
-                    String s = awaitPushUrl.get(i);
-                    if (SmallTool.isEqual(lastPushUrl, s)) {
-                        cutoff = i;
-                        break;
-                    }
-                }
-                awaitPushUrl = awaitPushUrl.subList(cutoff + 1, awaitPushUrl.size());
+        String url = "https://asia.archeage.com/news?lang=zh_TW";
+        String html = httpClientProxy.doGetTryForResult(url, Maps.newHashMap());
+        String pattern = "/news/{1}[\\d]+[?]{1}page={1}[\\d]+";
+        Matcher matcher = Pattern.compile(pattern).matcher(html);
 
-                System.out.println("==========================");
-                System.out.println("上次推送的消息:" + lastPushUrl);
-                for (String s : awaitPushUrl) {
-                    System.out.println("等待推送的消息:" + s);
-                }
-                System.out.println("==========================");
-
-                for (String newsUrl : awaitPushUrl) {
-                    url = "https://asia.archeage.com" + newsUrl;
-                    html = httpClientProxy.doGetForResult(url, Maps.newHashMap());
-                    pattern = "<article class=\"view\">{1}[\\d\\D]+</article>{1}";
-                    matcher = Pattern.compile(pattern).matcher(html);
-
-                    if (matcher.find()) {
-                        html = matcher.group();
-                        HtmlParser htmlParser = new HtmlParserImpl();
-                        htmlParser.loadHtml(html);
-                        ImageRenderer imageRenderer = new ImageRendererImpl(htmlParser);
-                        imageRenderer.saveImage("./1.png");
-
-                        url = kookProperties.getBaseUrl() + kookProperties.getUploadFile();
-                        CloseableHttpClient httpClient = HttpClients.createDefault();
-                        HttpPost httpPost = new HttpPost(url);
-                        httpPost.addHeader("Authorization", "Bot " + kookProperties.getToken());
-
-                        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-                        builder.addBinaryBody(
-                                "file",
-                                new FileInputStream("./1.png"),
-                                ContentType.MULTIPART_FORM_DATA,
-                                "1.png"
-                        );
-                        HttpEntity multipart = builder.build();
-                        httpPost.setEntity(multipart);
-                        CloseableHttpResponse response = httpClient.execute(httpPost);
-                        HttpEntity responseEntity = response.getEntity();
-                        String jsonString = EntityUtils.toString(responseEntity, "UTF-8");
-                        MapWrap result = JacksonUtil.fromJson(jsonString, MapWrap.class);
-                        String imgUrl = SmallTool.toString(result.get("data.url"), null);
-                        if (SmallTool.notEmpty(imgUrl)) {
-                            kookClient.channelImg("更新", imgUrl);
-                        }
-                        Thread.sleep(5000);
-                    }
-                    lastPushUrl = newsUrl;
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+        List<String> awaitPushUrl = Lists.newArrayList();
+        while (matcher.find()) {
+            awaitPushUrl.add(0, matcher.group());
+        }
+        if (SmallTool.isEmpty(lastPushUrl)) {
+            lastPushUrl = awaitPushUrl.get(awaitPushUrl.size() - 1);
+        }
+        int cutoff = 0;
+        for (int i = 0; i < awaitPushUrl.size(); i++) {
+            String s = awaitPushUrl.get(i);
+            if (SmallTool.isEqual(lastPushUrl, s)) {
+                cutoff = i;
+                break;
             }
         }
+        awaitPushUrl = awaitPushUrl.subList(cutoff + 1, awaitPushUrl.size());
+
+        System.out.println("==========================");
+        System.out.println("上次推送的消息:" + lastPushUrl);
+        for (String s : awaitPushUrl) {
+            System.out.println("等待推送的消息:" + s);
+        }
+        System.out.println("==========================");
+
+        for (String newsUrl : awaitPushUrl) {
+            url = "https://asia.archeage.com" + newsUrl;
+            html = httpClientProxy.doGetTryForResult(url, Maps.newHashMap());
+            pattern = "<article class=\"view\">{1}[\\d\\D]+</article>{1}";
+            matcher = Pattern.compile(pattern).matcher(html);
+
+            if (matcher.find()) {
+                html = matcher.group();
+                list.add(html);
+            }
+            lastPushUrl = newsUrl;
+        }
+        return list;
     }
 }
