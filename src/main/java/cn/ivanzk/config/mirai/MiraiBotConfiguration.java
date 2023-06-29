45228d2b1ce1515de8a7b88ba413ac402854eb30
zk <1184253234@qq.com>
2023-06-29 10:16:58 +0800

    修改缓存路径

--- src/main/java/cn/ivanzk/config/mirai/MiraiBotConfiguration.java
+++ src/main/java/cn/ivanzk/config/mirai/MiraiBotConfiguration.java
@@ -9,8 +9,10 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * QQ机器人配置
@@ -28,11 +30,12 @@ public class MiraiBotConfiguration {
 
     @Bean
     @ConditionalOnBean(MiraiBotProperties.class)
-    public BotConfiguration botConfiguration(MiraiBotProperties miraiBotProperties) {
+    public BotConfiguration botConfiguration(MiraiBotProperties miraiBotProperties) throws FileNotFoundException {
+        String classpath = ResourceUtils.getURL("classpath:").getPath();
         BotConfiguration botConfiguration = new BotConfiguration() {{
             noNetworkLog();
-            setCacheDir(new File("classpath:cache"));
-            fileBasedDeviceInfo("classpath:device.json");
+            setCacheDir(new File(classpath + "/cache"));
+            fileBasedDeviceInfo(classpath + "/device.json");
             setProtocol(miraiBotProperties.getProtocol());
             autoReconnectOnForceOffline();
         }};
